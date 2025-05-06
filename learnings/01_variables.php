7e818a04e670709b931913749adda1c060d0baef
Shahzad Farooq <crindalwalt@protonmail.com>
2025-05-06 15:58:32 +0500

    variables done

--- learnings/01_variables.php
+++ learnings/01_variables.php
@@ -1,6 +1,32 @@
 <?php
 
+/**
+ * Variables in PHP
+ * dont start with number, special character
+ * can start with underscore
+ * starts with a dollar sign $
+ * 
+ */
 
 
+/**
+ * Datatypes in php
+ * string
+ * number
+ * boolean
+ * array
+ */
 
-?>
+
+$name = "Shahzad Farooq";
+$linkedin = "@crindalwalt";
+$age = 21;
+echo $name;
+echo $age;
+$isMarried = true;
+
+$friends = ["Ayesha","Maryam","Saleha","Iqra"];
+$friends = array("Muhtshim","Zaid");
+echo var_dump($friends);
+// echo var_dump($isMarried);
+
